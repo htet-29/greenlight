dce6600e077ddc0dd277f63692ed8e67631df917
htethlaingwin <htethlaingwin.bsn29@gmail.com>
2025-12-24 17:31:28 +0630

    omitzero directive

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -4,10 +4,12 @@ import "time"
 
 type Movie struct {
 	ID        int64     `json:"id"`
-	CreatedAt time.Time `json:"created_at"`
+	CreatedAt time.Time `json:"-"` // '-' directive to hide
 	Title     string    `json:"title"`
-	Year      int32     `json:"year"`
-	Runtime   int32     `json:"runtime"`
-	Genres    []string  `json:"genres"`
+	Year      int32     `json:"year,omitzero"`
+	Runtime   int32     `json:"runtime,omitzero"`
+	Genres    []string  `json:"genres,omitzero"`
 	Version   int32     `json:"version"`
 }
+
+// `omitzero directive to hide the Year, Runtime and Genres fields in the output if and only if they are the relevant zero value for their type.`
