b3b71502effec0af9dcbc77d5a1bcc9228e906c4
htethlaingwin <htethlaingwin.bsn29@gmail.com>
2025-12-24 22:06:54 +0630

    Validating JSON input

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,6 +1,10 @@
 package data
 
-import "time"
+import (
+	"time"
+
+	"github.com/htet-29/greenlight/internal/validator"
+)
 
 type Movie struct {
 	ID        int64     `json:"id"`
@@ -13,3 +17,21 @@ type Movie struct {
 }
 
 // `omitzero directive to hide the Year, Runtime and Genres fields in the output if and only if they are the relevant zero value for their type.`
+
+func ValidateMovie(v *validator.Validator, movie *Movie) {
+	v.Check(movie.Title != "", "title", "must be provided")
+	v.Check(len(movie.Title) <= 500, "title", "must not be more than 500 bytes long")
+
+	v.Check(movie.Year != 0, "year", "must be provided")
+	v.Check(movie.Year >= 1888, "year", "must be greater than 1888")
+	v.Check(movie.Year <= int32(time.Now().Year()), "year", "must not be in the future")
+
+	v.Check(movie.Runtime != 0, "runtime", "must be provided")
+	v.Check(movie.Runtime > 0, "runtime", "must be a positive integer")
+
+	v.Check(movie.Genres != nil, "genres", "must be provided")
+	v.Check(len(movie.Genres) >= 1, "genres", "must contain at least 1 genre")
+	v.Check(len(movie.Genres) <= 5, "genres", "must not contain more than 5 genres")
+
+	v.Check(validator.Unique(movie.Genres), "genres", "must not contain duplicate values")
+}
